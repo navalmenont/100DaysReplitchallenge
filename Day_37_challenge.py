1149e2f1fd51d70d4c4fd7af0d3a8e5a332c79da
navalmenont <navalmenont@gmail.com>
2023-07-28 11:31:57 +0530

    Update Day_37_challenge.py

--- Day_37_challenge.py
+++ Day_37_challenge.py
@@ -13,4 +13,28 @@ print(f"Your StarWars Name is", {star_wars_name})
 
 
 
+def generate_star_wars_name():
+    # Ask the user for first and last names
+    full_name = input("Enter your first and last names (separated by a space): ")
+
+    # Split the input into first name and last name
+    first_name, last_name = full_name.split()
+
+    # Generate Star Wars first name
+    star_wars_first_name = f"{first_name[:3].capitalize()}{last_name[:3].capitalize()}"
+
+    # Ask the user for mother's maiden name and city of birth
+    maiden_name, city_of_birth = input("Enter your mother's maiden name and city of birth (separated by a space): ").split()
+
+    # Generate Star Wars last name
+    star_wars_last_name = f"{maiden_name[:2].capitalize()}{city_of_birth[-3:].capitalize()}"
+
+    # Print the Star Wars names
+    print(f"Your Star Wars name is: {star_wars_first_name} {star_wars_last_name}.")
+
+# Run the program
+generate_star_wars_name()
+
+
+
 
